6f7bfc02eccf128880d39d3e4f66ca73dc76a6af
sarena <sareena.aul@student.mahidol.edu>
2025-05-15 13:57:47 +0700

    implement the execution to check if the system command can be executed and testing

--- icsh.c
+++ icsh.c
@@ -1,17 +1,30 @@
 /* ICCS227: Project 1: icsh
- * Name:
- * StudentID:
+ * Name: Sareena Aulla
+ * StudentID: 6481197
  */
 
 #include "stdio.h"
+#include <sys/types.h>
 
 #define MAX_CMD_BUFFER 255
 
 int main() {
     char buffer[MAX_CMD_BUFFER];
     while (1) {
+        printf("Starting IC Shell\n");
         printf("icsh $ ");
         fgets(buffer, 255, stdin);
-        printf("you said: %s\n", buffer);
+        printf("%s\n", buffer);
     }
+}
+
+void execute(char **args) {
+    pid_t pid = fork();
+    if(pid == -1) {
+        printf("Error forking process\n");
+        return; 
+    } else if (pid == 0) {
+        if()
+    }
+
 }
